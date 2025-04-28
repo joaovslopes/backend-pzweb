ff9d95fe86eea17d4d997e0df61761d2472b18f0
joaovslopes <joao.sl99@hotmail.com>
2025-04-28 13:03:41 -0300

    ajuste para ficar 100% no site

--- routes/licenseRoutes.js
+++ routes/licenseRoutes.js
@@ -1,22 +1,28 @@
 // routes/licenseRoutes.js
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middlewares/auth');
 const {
-  createLauncherLicense,
-  getUserLicenses,
-  getLauncherLicenseInfo,
-  updateLauncherLicense,
-  deleteLicense,
-  verifyLauncherLicense
+    createLauncherLicense,
+    getLauncherLicenseInfo,
+    verifyLauncherLicense,
+    updateLauncherLicense,
+    deleteLicense
 } = require('../controllers/licenseController');
+const { checkLicenseMiddleware } = require('../middlewares/checkLicenseMiddleware');
 
+// Rota para criar a license do launcher
+router.post('/create', createLauncherLicense);
 
-router.post('/',           protect, createLauncherLicense);
-router.get('/',            protect, getUserLicenses);
-router.get('/:token/info', protect, getLauncherLicenseInfo);
-router.put('/:token',      protect, updateLauncherLicense);
-router.delete('/:id',      protect, deleteLicense);
-router.get('/:token/verify', verifyLauncherLicense); // pública se quiser
+// Rota para retornar informações da license do launcher
+router.get('/license-info/:token', checkLicenseMiddleware, getLauncherLicenseInfo);
+
+// Rota para verificar a license (opcional)
+router.get('/check/:token', checkLicenseMiddleware, verifyLauncherLicense);
+
+// Rota para atualizar a license (altera themeUrl ou updateUrl)
+router.put('/update/:token', updateLauncherLicense);
+
+// Rota para deletar uma license (aqui usamos o ID da license)
+router.delete('/delete/:id', deleteLicense);
 
 module.exports = router;
